f0abcf0393b4e7e5038d6b6046db07e9c25d6097
snahke <nikitakrh@mailbox.tu-berlin.de>
2018-01-09 15:55:35 +0100

    preprocessing done?

--- src/main/java/HomeworkBI1.scala
+++ src/main/java/HomeworkBI1.scala
@@ -1,5 +1,8 @@
-import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.fpm.FPGrowth
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
 
 object HomeworkBI1 {
 
@@ -12,37 +15,60 @@ object HomeworkBI1 {
       .getOrCreate()
     import spark.implicits._
 
+    val customSchema = StructType(Array(
+      StructField("InvoiceNo", StringType, true),
+      StructField("StockCode", StringType, true),
+      StructField("Description", StringType, true),
+      StructField("Quantity", IntegerType, true),
+      StructField("InvoiceDate", TimestampType, true),
+      StructField("UnitPrice", DoubleType, true),
+      StructField("CustomerID", IntegerType, true),
+      StructField("Country", StringType, true)))
+
     def readExcel(file: String): DataFrame = spark.read
       .format("com.crealytics.spark.excel")
       .option("useHeader", "true") // Required
       .option("treatEmptyValuesAsNulls", "true") // Optional, default: true
-      .option("inferSchema", "true") // Optional, default: false
+      .option("inferSchema", "false") // Optional, default: false
       .option("addColorColumns", "false") // Optional, default: false
       .option("maxRowsInMemory", 20) // Optional, default None. If set, uses a streaming reader which can help with big files
       .option("excerptSize", 10) // Optional, default: 10. If set and if schema inferred, number of rows to infer schema from
       .option("path", file)
+      .schema(customSchema)
       .load()
 
     val data = readExcel("src/main/resources/Online Retail.xlsx")
-      .selectExpr("cast(InvoiceNo as int) InvoiceNo",
-      "StockCode",
-      "Description",
-      "cast(Quantity as int) Quantity",
-      "InvoiceDate",
-      "UnitPrice",
-      "cast(CustomerID as int) CustomerID",
-      "Country")
-
-    val transactions: Dataset[(Int, String)] = data.map(s => (s.getAs[Int]("InvoiceNo"), s.getAs[String]("StockCode")))
-    transactions.show()
 
     // Preprocessing
     // TODO: make set of stockcodes for every transaction-number
 
-    // val transactions: RDD[Array[String]] = data.map(s => s.trim.split(' '))
+    val transactions = data
+      .select($"InvoiceNo", $"StockCode")
+      .groupBy($"InvoiceNo")
+      .agg(
+        collect_list("StockCode") as "StockCode"
+      )
+      .show()
 
     // Mining
     // TODO: Frequent itemset algorithm
+
+    // Frequent 1-itemset
+    val groupedDataByItem = data
+      .select("InvoiceNo", "StockCode")
+      .groupBy("StockCode")
+      .count()
+      .filter($"count" >= 2)
+      .show()
+
+//    val fpg = new FPGrowth()
+//      .setMinSupport(0.2)
+//      .setNumPartitions(10)
+//    val model = fpg.run()
+//
+//    model.freqItemsets.collect().foreach { itemset =>
+//      println(s"${itemset.items.mkString("[", ",", "]")},${itemset.freq}")
+//    }
 
     // Postprocessing
     // TODO: stockcodes to description & print description of frequent products
